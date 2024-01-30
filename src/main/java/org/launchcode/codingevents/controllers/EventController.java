f22d1ef1516889d0115a38555e63a9d5cb7ee272
Daniyal Syed <daniyal.syed@live.com>
2024-01-29 23:34:53 -0600

    delete model object

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -40,7 +40,24 @@ public class EventController {
                               @RequestParam String eventDescription) {
         EventData.add(new Event(eventName, eventDescription));
         return "redirect:/events";
+    }
 
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds !=null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:/events";
     }
 
 }
